dbbe6ad23aa310952e0c589d09b6626ecc1ad44a
lahiruthpala <lahiruthpala@gmail.com>
2026-01-17 19:29:58 +0530

    vercel deployment

---
+++ api/base.php
@@ -0,0 +1,8 @@
+<?php
+
+$base = "https://apply.aiesec.lk/";
+if ($_SERVER['SERVER_NAME'] == "localhost"){
+    $base = "https://localhost/aiesec.lk/apply/";
+}
+
+?>
---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$gcaptcha_private = "6LfddL4UAAAAAOPEYcGmekOu4StWaAvdS1vlzCX4";
+
+?>
